8fa9d1ae95d660d57b265a9c9ac4426c048dbea1
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-09-29 19:45:59 -0300

    Upload do Script

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = {
+    "linkedin": "https://www.youtube.com/?gl=BR",
+    "github": "https://github.com/L4r1ss4Cr4v0/",
+}
+
+function goLink(site){
+    window.open(links[site], "_blank"); 
+}
+
+var x = "informática" + 10 + 10
+console.log(x)
