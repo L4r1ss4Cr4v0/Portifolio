61ad5334aa1d42a6d5ab561acbd5bf9b92fc5613
L4r1ss4Cr4v0 <larissacravoccamara@gmail.com>
2024-09-30 13:13:17 -0300

    Progressão na construção do site

---
+++ script.js
@@ -0,0 +1,8 @@
+const links = {
+    "linkedin": "https://www.youtube.com/?gl=BR",
+    "github": "https://github.com/L4r1ss4Cr4v0/",
+}
+
+function goLink(site){
+    window.open(links[site], "_blank"); 
+}
