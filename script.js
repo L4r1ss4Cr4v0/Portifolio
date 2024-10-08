69689ce8b1d81184138267bcb0dbd5ba2a108574
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-10-07 21:33:33 -0300

    Progresso no desenvolvimento do site

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ const links = {
 function goLink(site){
     window.open(links[site], "_blank"); 
 }
+
+// teste de função 
+function flipCard(id_front, id_back){
+    console.log("Aqui");
+    document.getElementById('proj-um').classList.add('cardToFlip')
+    document.getElementById(id_front).classList.add('invisible');
+    document.getElementById(id_back).classList.remove('invisible');
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:false,
+    dots:false,
+    autoplay:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items:2.3
+        }
+    }
+  })
