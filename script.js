f8399a8b9a56dc0cc4e11d87399b543c89ad795d
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-10-15 12:33:18 -0300

    Criação da função darkMode

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const links = {
-    "linkedin": "https://www.youtube.com/?gl=BR",
+    "linkedin": "https://www.linkedin.com/in/larissa-cravo-154b66306/",
     "github": "https://github.com/L4r1ss4Cr4v0/",
 }
 
@@ -10,7 +10,17 @@ function goLink(site){
 function flipCard(id_front, id_back, cardElement) {
     document.getElementById(id_front).classList.toggle('invisible');
     document.getElementById(id_back).classList.toggle('invisible');
-    cardElement.classList.toggle('flipped')
+    cardElement.classList.toggle('flipped');
+}
+
+function darkMode(){
+    document.body.classList.toggle('darkMode');
+    if (document.body.classList == 'darkMode') {
+        document.getElementById("switch").classList.replace("bi-moon-stars","bi-brightness-high");
+    }
+    else{
+        document.getElementById("switch").classList.replace("bi-brightness-high","bi-moon-stars");
+    }
 }
 
 
